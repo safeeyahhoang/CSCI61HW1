ad1ae76dd394b8da7fdb7073a627cfb91df45c6c
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-09-29 10:03:56 -0700

    user class (x)

---
+++ user.h
@@ -0,0 +1,25 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <set>
+
+class User {
+private:
+    int id_;
+    std::string name_;
+    int year_;
+    int zip_;
+    std::set<int> friends_;
+
+public:
+    User();
+    User(int id, std::string name, int year, int zip, std::set<int> friends);
+    int getId() const;
+    std::string getName() const;
+    int getYear() const;
+    int getZip() const;
+    std::set<int>& getFriends();
+};
+
+#endif
