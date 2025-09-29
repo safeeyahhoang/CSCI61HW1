7ed200d2ca06b1027c2319f6dbf6b154f98fde9a
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-09-29 11:56:34 -0700

    imple user class

---
+++ user.cpp
@@ -0,0 +1,13 @@
+#include "user.h"
+
+User::User()
+    : id_(-1), name_(""), year_(1934), zip_(89591) {}
+
+User::User(int id, std::string name, int year, int zip, std::set<int> friends)
+    : id_(id), name_(name), year_(year), zip_(zip), friends_(friends) {}
+
+int User::getId() const { return id_; }
+std::string User::getName() const { return name_; }
+int User::getYear() const { return year_; }
+int User::getZip() const { return zip_; }
+std::set<int>& User::getFriends() { return friends_; }
