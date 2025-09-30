f8f8e93a672a8ebe832e6f4d846c4a9ebc5483dd
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-09-29 23:49:17 -0700

    edit network.cpp

--- network.cpp
+++ network.cpp
@@ -13,3 +13,18 @@ std::string Network::trim(const std::string& str) {
     size_t end = str.find_last_not_of(" \t\r\n");
     return str.substr(start, end);
 }
+
+User* Network::getUser(int id) {
+    if (id < 0 || id >= users_.size()) return nullptr;
+    return users_[id];
+}
+
+void Network::addUser(User* u) {
+    users_.push_back(u); 
+}
+
+int Network::getId(std::string name) {
+    for (size_t i = 0; i < users_.size(); i++)
+        if (users_[i]->getName() == name) return i;
+    return -1;
+}
