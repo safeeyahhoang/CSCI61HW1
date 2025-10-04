d224aae907c0eb62057f2ccefde616bf700d4c3f
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-10-04 12:14:22 -0700

    final

--- user.cpp
+++ user.cpp
@@ -6,11 +6,26 @@ User::User()
 User::User(int id, std::string name, int year, int zip, std::set<int> friends)
     : id_(id), name_(name), year_(year), zip_(zip), friends_(friends) {}
 
-int User::getId() const { return id_; }
-std::string User::getName() const { return name_; }
-int User::getYear() const { return year_; }
-int User::getZip() const { return zip_; }
-std::set<int>& User::getFriends() { return friends_; }
+int User::getId() const {
+    return id_;
+}
+
+std::string User::getName() const {
+    return name_;
+}
+
+int User::getYear() const {
+    return year_;
+}
+
+int User::getZip() const {
+    return zip_;
+}
+
+std::set<int>& User::getFriends() {
+    return friends_;
+}
+
 void User::addFriend(int id) {
     if (friends_.find(id) == friends_.end()) {
         friends_.insert(id);
