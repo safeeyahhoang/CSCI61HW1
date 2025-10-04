c1015bd6983fd288ad697377e47740b922477090
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-10-04 16:28:19 -0700

    final

--- user.h
+++ user.h
@@ -13,15 +13,41 @@ private:
     std::set<int> friends_;
 
 public:
+    // pre: none
+    // post: make one user, use default year=1934 and zip=89591
     User();
+
+    // pre: none
+    // post: make user with info given by parameter
     User(int id, std::string name, int year, int zip, std::set<int> friends);
+
+    // pre: none
+    // post: return id of user
     int getId() const;
+
+    // pre: none
+    // post: return name of user
     std::string getName() const;
+
+    // pre: none
+    // post: return year born of user
     int getYear() const;
+
+    // pre: none
+    // post: return zip number of user
     int getZip() const;
+
+    // pre: none
+    // post: return friend list (can change it)
     std::set<int>& getFriends();
+
+    // pre: none
+    // post: add new friend id if not there yet
     void addFriend(int id);
+
+    // pre: none
+    // post: delete friend id if already in list
     void deleteFriend(int id);
 };
 
-#endif
+#endif // USER_H
